9c53be4b15284c0748906d4c3897274e9edadc99
zheng chen <edward.eric.zheng@gmail.com>
2012-01-11 00:08:15 +0800

    new frame

--- DOMFrame.py
+++ DOMFrame.py
@@ -1,41 +1,30 @@
 import wx
 from xml.dom import minidom
-class TestFrame(wx.Frame):
+class DOMFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, title="simple tree", size=(400,500))
+        wx.Frame.__init__(self, None, title="XML Reader", size=(450,350))
         # Create the tree
-        self.tree = wx.TreeCtrl(self)
+        treePanel = wx.Panel(self, -1)
+        textPanel = wx.Panel(self, -1)
         
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        
+        self.tree = wx.TreeCtrl(treePanel )
+        self.display = wx.StaticText(textPanel, style=wx.ALIGN_LEFT)      
         root = minidom.parse('DataMaintenance.xml').documentElement
-        
         self.addNode(None, root)
         
-        #self.addtest(None, root)
+        self.tree.Bind(wx.EVT_TREE_SEL_CHANGED, self.onSelectChanged, self.tree)
+        self.tree.Expand(self.tree.GetRootItem())
         
-        # Add a root node
-        #root = self.tree.AddRoot("wx.Object")
-        # Add nodes from our data set
-        #self.AddTreeNodes(root, data.tree)
-        #self.addNode(None, reader.loadfile('DataMaintenance.xml'))
-        # Bind some interesting events
-        self.Bind(wx.EVT_TREE_ITEM_EXPANDED, self.OnItemExpanded, self.tree)
-        self.Bind(wx.EVT_TREE_ITEM_COLLAPSED, self.OnItemCollapsed, self.tree)
-        self.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, self.tree)
-        self.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnActivated, self.tree)
-        # Expand the first level
-        #self.tree.Expand(root)
-    
-    def AddTreeNodes(self, parentItem, items):
-        """
-        Recursively traverses the data structure, adding tree nodes to
-        match it.
-        """
-        for item in items:
-            if type(item) == str:
-                self.tree.AppendItem(parentItem, item)
-            else:
-                newItem = self.tree.AppendItem(parentItem, item[0])
-                self.AddTreeNodes(newItem, item[1])
+        vbox.Add(self.tree, 1, wx.EXPAND)
+        hbox.Add(treePanel, 1, wx.EXPAND)
+        hbox.Add(textPanel, 1, wx.EXPAND)
+        
+        treePanel.SetSizer(vbox)
+        self.SetSizer(hbox)
+        self.Center()
     
     def addNode(self, parentNode, node):
         if node.nodeType == node.ELEMENT_NODE:
@@ -45,29 +34,16 @@ class TestFrame(wx.Frame):
                 x = self.tree.AddRoot(node.nodeName)
             if node.hasAttributes():
                 for a in node.attributes.keys():
-                    value = node.attributes.get(a).value
-                    value = unicode.strip(value)
-                    self.tree.AppendItem(x, a + "=" + value)
+                    self.tree.AppendItem(x, a + "=" + unicode.strip(node.attributes.get(a).value))
             if node.hasChildNodes():
                 for c in node.childNodes:
-                    self.addNode(x, c)             
+                    self.addNode(x, c)
+    
+    def onSelectChanged(self,evt):
+        item =  evt.GetItem()
+        self.display.SetLabel(self.tree.GetItemText(item)) 
 
-    def GetItemText(self, item):
-        if item:
-            return self.tree.GetItemText(item)
-        else:
-            return ""
-
-    def OnItemExpanded(self, evt):
-        print "OnItemExpanded: ", self.GetItemText(evt.GetItem())
-
-    def OnItemCollapsed(self, evt):
-        print "OnItemCollapsed:", self.GetItemText(evt.GetItem())
-    def OnSelChanged(self, evt):
-        print "OnSelChanged:   ", self.GetItemText(evt.GetItem())
-    def OnActivated(self, evt):
-        print "OnActivated:    ", self.GetItemText(evt.GetItem())
 app = wx.PySimpleApp(redirect=True)
-frame = TestFrame()
+frame = DOMFrame()
 frame.Show()
 app.MainLoop()
